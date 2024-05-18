559f64f0aa988815a61f2a006151de2d5fe9914f
Lena things <lenaiiie.cookie@hotmail.com>
2024-05-18 17:24:25 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,6 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-name 'BetterFaces'
-description 'Face Pack for MP-Peds. Orginaly created by BAHO'
-version 'v1.0'
-author '1iq_'
-
-files {
-'*.meta'
-}
+name 'Cookie-Faces'
+description 'Upscaled Faces Textures And Normals'
+version 'v1.0.0'
+author 'Cookiiie'
